5970cf5da346b41c1957da5173de0cff85949ff3
Fichey <czarekpolk@gmail.com>
2025-09-19 09:15:29 +0200

    try 4

--- Dockerfile
+++ Dockerfile
@@ -14,17 +14,13 @@ RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd6
     && apt-get install -y ./google-chrome-stable_current_amd64.deb \
     && rm google-chrome-stable_current_amd64.deb
 
-
+    
 # Install Chromedriver matching Chrome version
-RUN export CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f3 | cut -d. -f1) && \
-    echo "Installed Chrome Major Version: $CHROME_VERSION" && \
-    # Download matching Chromedriver for that version
-    wget -q "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROME_VERSION.0.0.0/linux64/chromedriver-linux64.zip" -O /tmp/chromedriver.zip && \
-    unzip /tmp/chromedriver.zip -d /usr/local/bin/ && \
-    mv /usr/local/bin/chromedriver-linux64/chromedriver /usr/local/bin/chromedriver && \
-    rm -rf /usr/local/bin/chromedriver-linux64 /tmp/chromedriver.zip && \
-    chmod +x /usr/local/bin/chromedriver
-
+RUN CHROMEDRIVER_VERSION=$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE) \
+    && wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip \
+    && unzip /tmp/chromedriver.zip -d /usr/local/bin/ \
+    && rm /tmp/chromedriver.zip \
+    && chmod +x /usr/local/bin/chromedriver
 
 # Cleanup apt cache
 RUN apt-get clean && rm -rf /var/lib/apt/lists/*
--- get_products.py
+++ get_products.py
@@ -24,12 +24,31 @@ def main():
 
 
     # Change to your chromedriver path if necessary
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.set_capability('browserless:token', os.environ['BROWSER_TOKEN'])
+    # Set args similar to puppeteer's for best performance
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-background-timer-throttling")
+    chrome_options.add_argument("--disable-backgrounding-occluded-windows")
+    chrome_options.add_argument("--disable-breakpad")
+    chrome_options.add_argument("--disable-component-extensions-with-background-pages")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-features=TranslateUI,BlinkGenPropertyTrees")
+    chrome_options.add_argument("--disable-ipc-flooding-protection")
+    chrome_options.add_argument("--disable-renderer-backgrounding")
+    chrome_options.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
+    chrome_options.add_argument("--force-color-profile=srgb")
+    chrome_options.add_argument("--hide-scrollbars")
+    chrome_options.add_argument("--metrics-recording-only")
+    chrome_options.add_argument("--mute-audio")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Remote(
+        command_executor=os.environ['BROWSER_WEBDRIVER_ENDPOINT'],
+        options=chrome_options
+    )
     WebDriverWait(driver, 15)
     # Step 1: Go to the collections page
     COLLECTION_URL = "https://primacol.com/en-pl/collections/collections"
