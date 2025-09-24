bbf37bac19b9658dfb8883656da7f95ed869149c
Fichey <czarekpolk@gmail.com>
2025-09-24 10:27:35 +0200

    added PORT and commented app.run

--- app.py
+++ app.py
@@ -53,6 +53,6 @@ def generate_article():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 8000))
+#     app.run(host='0.0.0.0', port=port, debug=True)
